38a77c6ee19f1782001534030f144848a10b4e79
Malshini2000Nsbm <140839939+Malshini2000Nsbm@users.noreply.github.com>
2023-07-28 22:24:21 +0530

    Add files via upload

---
+++ Lab 01-Q2/Lab 01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius, area;
+            Console.WriteLine("Enter the radius: ");
+
+            radius = double.Parse(Console.ReadLine());
+            area = Math.PI * radius * radius;
+
+            Console.WriteLine("area is " + area);
+
+            Console.ReadKey();
+        }
+    }
+}

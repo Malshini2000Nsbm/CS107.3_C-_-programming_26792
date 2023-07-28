db1af899e14ca8b22b548fde0746ec49200b6624
Malshini2000Nsbm <140839939+Malshini2000Nsbm@users.noreply.github.com>
2023-07-28 22:39:39 +0530

    Add files via upload

---
+++ Lab 02-Q6/Lab 02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 10;
+            int sum = 0;
+            Console.WriteLine($" enter {count} numbers");
+            for (int i = 0; i < count; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+            }
+            float avg = (float)sum / count;
+            Console.WriteLine($"sum:{sum}");
+            Console.WriteLine($"average:{avg}");
+            Console.ReadLine();
+        }
+    }
+}

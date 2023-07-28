8135083112aa3cc3a85f685fda5c37176dcbb15a
Malshini2000Nsbm <140839939+Malshini2000Nsbm@users.noreply.github.com>
2023-07-28 22:19:22 +0530

    Add files via upload

---
+++ Lab 01-Q1/Lab 01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string batch;
+
+            Console.WriteLine("Enter Your Name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter Your batch: ");
+            batch = Console.ReadLine();
+
+            Console.WriteLine("Your name is " + name);
+            Console.WriteLine("Your batch is " + batch);
+
+            Console.ReadKey();
+        }
+    }
+}

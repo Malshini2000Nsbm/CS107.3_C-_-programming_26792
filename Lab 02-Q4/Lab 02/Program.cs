d269948b2bf76830b212aa616c0ae80c83dd2615
Malshini2000Nsbm <140839939+Malshini2000Nsbm@users.noreply.github.com>
2023-07-28 22:34:24 +0530

    Add files via upload

---
+++ Lab 02-Q4/Lab 02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int no;
+            Console.WriteLine("Enter whole number: ");
+            
+            no = int.Parse(Console.ReadLine());
+
+            if (no > 70)
+            {
+                Console.WriteLine("PASS");
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            Console.ReadLine();
+        }
+    }
+}

854194a8e28a40622b4a84618e6fce302f70aba1
Malshini2000Nsbm <140839939+Malshini2000Nsbm@users.noreply.github.com>
2023-07-28 22:40:52 +0530

    Add files via upload

---
+++ Lab 02-Q8/Lab 02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            do
+            {
+                Console.WriteLine("enter number: ");
+                x = int.Parse(Console.ReadLine());
+            }
+            while (x > 0);
+            Console.ReadLine();
+        }
+    }
+}

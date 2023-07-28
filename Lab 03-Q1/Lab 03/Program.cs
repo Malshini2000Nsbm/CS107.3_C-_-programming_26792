7f05075a8b2b155aab5028b40ce6fc223b659c80
Malshini2000Nsbm <140839939+Malshini2000Nsbm@users.noreply.github.com>
2023-07-28 23:07:15 +0530

    Add files via upload

---
+++ Lab 03-Q1/Lab 03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int no;
+            Console.WriteLine("Enter an integer number ");
+            no = int.Parse(Console.ReadLine());
+            if (no % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}

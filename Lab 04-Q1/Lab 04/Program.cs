2ea9b8a3addb7593359d4229eb7e9ed60c431a84
Malshini2000Nsbm <140839939+Malshini2000Nsbm@users.noreply.github.com>
2023-07-28 23:16:22 +0530

    Add files via upload

---
+++ Lab 04-Q1/Lab 04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+            int sum = 0;
+            while (count <= 10)
+            {
+                Console.WriteLine("Enter value for : " + count);
+                string input = Console.ReadLine();
+                int int_input = int.Parse(input);
+                sum = sum + int_input;
+                count++;
+            }
+            Console.WriteLine("The average is: " + (sum / 10));
+            Console.ReadLine();
+        }
+    }
+}

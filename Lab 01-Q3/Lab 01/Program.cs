6d615c0f90f650379e3dc61567c955267f9e4921
Malshini2000Nsbm <140839939+Malshini2000Nsbm@users.noreply.github.com>
2023-07-28 22:26:22 +0530

    Add files via upload

---
+++ Lab 01-Q3/Lab 01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, sum;
+
+            Console.WriteLine("Enter the first number: ");
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            num2 = double.Parse(Console.ReadLine());
+
+            sum = num1 + num2;
+
+            Console.WriteLine("The summation is " + sum);
+
+            Console.ReadKey();
+        }
+    }
+}

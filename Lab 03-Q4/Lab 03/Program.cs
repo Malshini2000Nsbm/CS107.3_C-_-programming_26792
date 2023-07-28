177eb6fa4b8938c26f0138933783bccc4cf62e94
Malshini2000Nsbm <140839939+Malshini2000Nsbm@users.noreply.github.com>
2023-07-28 23:12:43 +0530

    Add files via upload

---
+++ Lab 03-Q4/Lab 03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, sum = 0;
+            Console.WriteLine("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i += 2)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine("Sum of odd numbers from 1 to {n} is: " + sum);
+            Console.ReadLine();
+
+        }
+    }
+}

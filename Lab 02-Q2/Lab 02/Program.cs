74305587b5c1790ddb14aea6328e61927484cd60
Malshini2000Nsbm <140839939+Malshini2000Nsbm@users.noreply.github.com>
2023-07-28 22:31:14 +0530

    Add files via upload

---
+++ Lab 02-Q2/Lab 02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double height, width;
+
+            Console.WriteLine("Enter the height: ");
+            height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the width: ");
+            width = double.Parse(Console.ReadLine());
+
+            double area = width * height;
+
+            Console.WriteLine("The area is " + area);
+
+            double perimeter = (2 * width) + (2 * height);
+
+            Console.WriteLine("The perimeter is " + perimeter);
+
+            Console.ReadLine();
+        }
+    }
+}

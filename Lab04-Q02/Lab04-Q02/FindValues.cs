cece4d2d4c2608a30c49064e593aaf4e528cc48d
Malshini Sashintha <140839939+Malshini2000Nsbm@users.noreply.github.com>
2023-07-31 19:58:51 +0530

    Add files via upload

---
+++ Lab04-Q02/Lab04-Q02/FindValues.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04_Q02
+{
+    internal class FindValues
+    {
+        
+    public double FindArea(double radius)
+        {
+            double area = Math.PI * radius * radius;
+            return area;
+        }
+
+        public double FindCircumference(double radius)
+        {
+            double circumference = 2 * Math.PI * radius;
+            return circumference;
+        }
+    }
+}
+
---
+++ Lab04-Q02/Lab04-Q02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04_Q02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.Write("Enter the radius of the circle: ");
+                double radius = double.Parse(Console.ReadLine());
+
+                FindValues finder = new FindValues();
+                double area = finder.FindArea(radius);
+                double circumference = finder.FindCircumference(radius);
+
+                Console.WriteLine($"The area of the circle with radius {radius} is: {area}");
+                Console.WriteLine($"The circumference of the circle with radius {radius} is: {circumference}");
+            }
+        }
+    }
+}

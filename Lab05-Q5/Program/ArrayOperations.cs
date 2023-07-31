92f9ce01f206679de393410c66d2d9c142b03787
Malshini Sashintha <140839939+Malshini2000Nsbm@users.noreply.github.com>
2023-07-31 20:07:52 +0530

    Add files via upload

---
+++ Lab05-Q5/Program/ArrayOperations.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class ArrayOperations
+    {
+        public int FindMinimum(int[] array)
+        {
+            int min = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            return min;
+        }
+
+        public int FindMaximum(int[] array)
+        {
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+            return max;
+        }
+
+        public double FindAverage(int[] array)
+        {
+            double sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum / array.Length;
+        }
+
+        public int[] ReverseArray(int[] array)
+        {
+            int[] reversedArray = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                reversedArray[i] = array[array.Length - 1 - i];
+            }
+            return reversedArray;
+        }
+    }
+}
---
+++ Lab05-Q5/Program/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+   
+            
+                int[] array = new int[10];
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write($"Enter value {i + 1}: ");
+                    array[i] = int.Parse(Console.ReadLine());
+                }
+
+                ArrayOperations arrayOps = new ArrayOperations();
+
+                int min = arrayOps.FindMinimum(array);
+                int max = arrayOps.FindMaximum(array);
+                double average = arrayOps.FindAverage(array);
+                int[] reversedArray = arrayOps.ReverseArray(array);
+
+                Console.WriteLine($"Minimum value: {min}");
+                Console.WriteLine($"Maximum value: {max}");
+                Console.WriteLine($"Average value: {average}");
+                Console.WriteLine("Reversed array:");
+                foreach (int value in reversedArray)
+                {
+                    Console.Write(value + " ");
+                }
+            
+        }
+    }
+}

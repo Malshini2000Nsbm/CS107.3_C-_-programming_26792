bfe8483f9debbb7ae41b1b743f21373c07ad09b5
Malshini Sashintha <140839939+Malshini2000Nsbm@users.noreply.github.com>
2023-07-31 20:02:44 +0530

    Add files via upload

---
+++ Lab05-Q4/Lab05-Q4/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab05_Q4
+{
+    internal class MyClass
+    {
+        private void SayHello()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Lab05-Q4/Lab05-Q4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab05_Q4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                MyClass myObject = new MyClass();
+
+                // Attempt to access the private method.
+                // This will cause a compile-time error since private methods cannot be accessed from outside the class.
+                myObject.SayHello();
+            }
+        }
+    }
+}

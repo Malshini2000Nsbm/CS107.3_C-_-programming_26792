f4fb02fc59e4366123a78a9bd29f12944cdb31ad
Malshini Sashintha <140839939+Malshini2000Nsbm@users.noreply.github.com>
2023-07-31 21:47:12 +0530

    Add files via upload

---
+++ Lab07-Q8/Lab07-Q8/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07_Q8
+{
+    internal class Animal
+    {
+        public void AnimalMethod()
+        {
+            Console.WriteLine("I am an Animal");
+        }
+    }
+}
---
+++ Lab07-Q8/Lab07-Q8/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07_Q8
+{
+    internal class Dog
+    {
+        public void DogMethod()
+        {
+            Console.WriteLine("I have four legs");
+        }
+    }
+}

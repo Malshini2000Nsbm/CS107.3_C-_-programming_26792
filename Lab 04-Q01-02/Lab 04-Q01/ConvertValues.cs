b99adcd3fc41f39b4906e0a6eb89b41f2c0fba8a
Malshini Sashintha <140839939+Malshini2000Nsbm@users.noreply.github.com>
2023-07-31 19:46:33 +0530

    Add files via upload

---
+++ Lab 04-Q01-02/Lab 04-Q01/ConvertValues.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_04_Q01
+{
+    internal class ConvertValues
+    {
+
+            public void kilometerTOmeter(double kilometers)
+            {
+                double meters = kilometers * 1000;
+                Console.WriteLine("{0} kilometers is equal to {1} meters.", kilometers, meters);
+            }
+        
+
+    }
+}
---
+++ Lab 04-Q01-02/Lab 04-Q01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_04_Q01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertValues objConverteValues = new ConvertValues();
+
+            Console.Write("Enter the distance in kilometers: ");
+            double kilometers = Convert.ToDouble(Console.ReadLine());
+
+            objConverteValues.kilometerTOmeter(kilometers);
+        }
+    }
+}

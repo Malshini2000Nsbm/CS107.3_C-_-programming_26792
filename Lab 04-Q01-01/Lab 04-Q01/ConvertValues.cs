3e61b1652f3eee3e2a5332661dfba60ac6357a85
Malshini Sashintha <140839939+Malshini2000Nsbm@users.noreply.github.com>
2023-07-31 19:35:05 +0530

    Add files via upload

---
+++ Lab 04-Q01-01/Lab 04-Q01/ConvertValues.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_04_Q01
+{
+    internal class ConvertValues
+    {
+        public void kilometerTOmeter()
+        {
+            double km;
+
+            Console.WriteLine("Enter the km value: ");
+            km= double.Parse(Console.ReadLine());
+
+            double cm = km * 100000;
+
+            Console.WriteLine("cm value is: " + cm);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab 04-Q01-01/Lab 04-Q01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_04_Q01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertValues objConvertValues=new ConvertValues();
+            objConvertValues.kilometerTOmeter();
+
+            objConvertValues.kilometerTOmeter();
+        }
+    }
+}
